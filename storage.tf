78fe2cf86f5327ea0d8802088d0ffa8a6c518ef4
Meruem96 <61518622+Meruem96@users.noreply.github.com>
2021-03-04 18:34:33 +0100

    Update storage.tf

--- storage.tf
+++ storage.tf
@@ -45,6 +45,5 @@ resource "azurerm_storage_account_network_rules" "test" {
 
   default_action             = "Allow"
   ip_rules                   = ["127.0.0.1"]
-  virtual_network_subnet_ids = [azurerm_subnet.example.id]
   bypass                     = ["Metrics"]
 }
