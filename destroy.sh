6a98eba7ca5f65402bd407122e41e913398363fa
Meruem96 <61518622+Meruem96@users.noreply.github.com>
2021-03-04 19:18:44 +0100

    Delete destroy.sh

--- destroy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source exports
-
-az group delete --resource-group $TERRAGOAT_RESOURCE_GROUP --yes
-az group delete --resource-group "terragoat-"$TF_VAR_environment --yes 
-az group delete --resource-group "NetworkWatcherRG" --yes
