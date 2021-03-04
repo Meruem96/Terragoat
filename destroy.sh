bf7b625f1842273551f97bc4e2426466cceaa323
Meruem96 <61518622+Meruem96@users.noreply.github.com>
2021-03-04 19:05:35 +0100

    Update destroy.sh

--- destroy.sh
+++ destroy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source exports
 
-if ![ -f "destroyoutput.log" ]; then touch "destroyoutput.log"; fi
+if ! [ -f "destroyoutput.log" ]; then touch "destroyoutput.log"; fi
 az group delete --resource-group $TERRAGOAT_RESOURCE_GROUP 2>&1 destroyoutput.log && echo "$TERRAGOAT_RESOURCE_GROUP resource group deleted"
 az group delete --resource-group "terragoat-"$TF_VAR_environment 2>&1 destroyoutput.log && echo "terragoat-"$TF_VAR_environment" resource group deleted"
 az group delete --resource-group "NetworkWatcherRG" 2>&1 destroyoutput.log && echo "NetworkWatcherRG resource group deleted 
