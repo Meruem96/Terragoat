787ce34006631c47235587437019c4a4fe2d9929
Meruem96 <61518622+Meruem96@users.noreply.github.com>
2021-03-04 17:44:14 +0100

    Update storage.tf

--- storage.tf
+++ storage.tf
@@ -43,7 +43,7 @@ resource "azurerm_storage_account_network_rules" "test" {
   resource_group_name  = azurerm_resource_group.example.name
   storage_account_name = azurerm_storage_account.example.name
 
-  default_action = "Deny"
+  default_action = "Allow"
   ip_rules       = ["127.0.0.1"]
   bypass         = ["Metrics"]
 }
