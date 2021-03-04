937f58cb8457e90b2e2e9bf23491dc373b6ea4f1
Meruem96 <61518622+Meruem96@users.noreply.github.com>
2021-03-04 19:54:14 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,16 +6,16 @@ export TF_VAR_environment="dev"
 export TF_VAR_region="westus"
 
 # Create resource group
-az group create --location $TF_VAR_region --name $TERRAGOAT_RESOURCE_GROUP >> setupoutput.log && echo "Resource group created"
+#az group create --location $TF_VAR_region --name $TERRAGOAT_RESOURCE_GROUP >> setupoutput.log && echo "Resource group created"
 
 # Create storage account
-az storage account create --name $TERRAGOAT_STATE_STORAGE_ACCOUNT --resource-group $TERRAGOAT_RESOURCE_GROUP --location $TF_VAR_region --sku Standard_LRS --kind StorageV2 --https-only true --encryption-services blob >> setupoutput.log && echo "Storage account created"
+#az storage account create --name $TERRAGOAT_STATE_STORAGE_ACCOUNT --resource-group $TERRAGOAT_RESOURCE_GROUP --location $TF_VAR_region --sku Standard_LRS --kind StorageV2 --https-only true --encryption-services blob >> setupoutput.log && echo "Storage account created"
 
 # Get storage account key
-ACCOUNT_KEY=$(az storage account keys list --resource-group $TERRAGOAT_RESOURCE_GROUP --account-name $TERRAGOAT_STATE_STORAGE_ACCOUNT --query [0].value -o tsv) >> setupoutput.log && echo "Storage account key retreived"
+#ACCOUNT_KEY=$(az storage account keys list --resource-group $TERRAGOAT_RESOURCE_GROUP --account-name $TERRAGOAT_STATE_STORAGE_ACCOUNT --query [0].value -o tsv) >> setupoutput.log && echo "Storage account key retreived"
 
 # Create blob container
-az storage container create --name $TERRAGOAT_STATE_CONTAINER --account-name $TERRAGOAT_STATE_STORAGE_ACCOUNT --account-key $ACCOUNT_KEY >> setupoutput.log && echo "Blob container created"
+#az storage container create --name $TERRAGOAT_STATE_CONTAINER --account-name $TERRAGOAT_STATE_STORAGE_ACCOUNT --account-key $ACCOUNT_KEY >> setupoutput.log && echo "Blob container created"
 
 # Fetch object_id
 objectId=$(az ad signed-in-user show --query objectId)
@@ -25,6 +25,7 @@ echo """variable "object_id" {
   default     = $objectId
 }""" >> variables.tf
 
+exit
 echo "Terraform init ..."
 terraform init -reconfigure -backend-config="resource_group_name=$TERRAGOAT_RESOURCE_GROUP" \
     -backend-config "storage_account_name=$TERRAGOAT_STATE_STORAGE_ACCOUNT" \
