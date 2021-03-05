cdb3872354f79a9413e7e491fd23efccd310bf95
Meruem96 <61518622+Meruem96@users.noreply.github.com>
2021-03-05 10:03:54 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,24 +6,27 @@ export TF_VAR_environment="dev"
 export TF_VAR_region="westus"
 
 # Create resource group
-az group create --location $TF_VAR_region --name $TERRAGOAT_RESOURCE_GROUP >> setupoutput.log && echo "Resource group created"
+echo -n "Resource group ..."
+az group create --location $TF_VAR_region --name $TERRAGOAT_RESOURCE_GROUP >> setupoutput.log && echo "OK"
 
 # Create storage account
-az storage account create --name $TERRAGOAT_STATE_STORAGE_ACCOUNT --resource-group $TERRAGOAT_RESOURCE_GROUP --location $TF_VAR_region --sku Standard_LRS --kind StorageV2 --https-only true --encryption-services blob >> setupoutput.log && echo "Storage account created"
+echo -n "Storage account ..."
+az storage account create --name $TERRAGOAT_STATE_STORAGE_ACCOUNT --resource-group $TERRAGOAT_RESOURCE_GROUP --location $TF_VAR_region --sku Standard_LRS --kind StorageV2 --https-only true --encryption-services blob >> setupoutput.log && echo "OK"
 
 # Get storage account key
-ACCOUNT_KEY=$(az storage account keys list --resource-group $TERRAGOAT_RESOURCE_GROUP --account-name $TERRAGOAT_STATE_STORAGE_ACCOUNT --query [0].value -o tsv) >> setupoutput.log && echo "Storage account key retreived"
+echo -n "Storage account key ..."
+ACCOUNT_KEY=$(az storage account keys list --resource-group $TERRAGOAT_RESOURCE_GROUP --account-name $TERRAGOAT_STATE_STORAGE_ACCOUNT --query [0].value -o tsv) >> setupoutput.log && echo "OK"
 
 # Create blob container
-az storage container create --name $TERRAGOAT_STATE_CONTAINER --account-name $TERRAGOAT_STATE_STORAGE_ACCOUNT --account-key $ACCOUNT_KEY >> setupoutput.log && echo "Blob container created"
+echo -n "Blob container ..."
+az storage container create --name $TERRAGOAT_STATE_CONTAINER --account-name $TERRAGOAT_STATE_STORAGE_ACCOUNT --account-key $ACCOUNT_KEY >> setupoutput.log && echo "OK"
 
 # Fetch object_id
 objectId=$(az ad signed-in-user show --query objectId)
 if [ -f "variables.tf" ] && [ $(cat "variables.tf" | grep 'object_id' | wc -l) -eq 1 ]
 then
         echo -n "objectId variable already exists, updating ..."
-        sed -i 's/default.*/default     = '$objectId'/g' variables.tf
-        echo "OK"
+        sed -i 's/default.*/default     = '$objectId'/g' variables.tf && echo "OK"
 else
         echo -n "Pushing objectId into variables.tf ..."
         echo '
@@ -31,17 +34,17 @@ variable "object_id" {
   type        = string
   description = "The object ID of the current user"
   default     = '$objectId'
-}' >> variables.tf
+}' >> variables.tf && echo "OK"
 fi
 
 echo "Terraform init ..."
 terraform init -reconfigure -backend-config="resource_group_name=$TERRAGOAT_RESOURCE_GROUP" \
     -backend-config "storage_account_name=$TERRAGOAT_STATE_STORAGE_ACCOUNT" \
     -backend-config="container_name=$TERRAGOAT_STATE_CONTAINER" \
-    -backend-config "key=$TF_VAR_environment.terraform.tfstate" >> setupoutput.log && echo "Initialisation complete"
+    -backend-config "key=$TF_VAR_environment.terraform.tfstate" >> setupoutput.log && echo "OK"
 
 echo "Terraform plan ..."
-terraform plan > plan.log && echo "Plan saved as plan.log"
+terraform plan > plan.log && echo "OK (saved as plan.log)"
 
 read -p "Apply ? [Y/N]" resp
 
