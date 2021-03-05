cf4d69e58709bc24d298e31a3c367f7088734be0
Meruem96 <61518622+Meruem96@users.noreply.github.com>
2021-03-05 10:56:20 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,14 +7,6 @@ variable "subscription_id" {
   default     = null
 }
 
-output "current_subscription_display_name" {
-  value = data.azurerm_subscription.current.display_name
-}
-
-output "subscription_id_value" {
-  value = var.subscription_id
-}
-
 variable "location" {
   type    = string
   default = "East US"
