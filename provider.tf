d3dc92822af98f324976c3c87ac5e436320dbb05
meruem96 <clement.legoupil@gmail.com>
2021-03-11 09:43:02 +0100

    provider

--- provider.tf
+++ provider.tf
@@ -1,5 +1,6 @@
 provider "azurerm" {
   subscription_id = var.subscription_id
+  
   features {}
 }
 
@@ -7,10 +8,8 @@ provider "azurerm" {
 data "azurerm_client_config" "current" {}
 
 terraform {
-  required_providers {
-    version = ">= 2.0.0"
-  }
   required_version = ">=0.12.0"
   backend "azurerm" {
+    required_version = ">= 2.0.0"
   }
 }
