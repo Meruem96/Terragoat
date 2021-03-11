504608b0113502572df0d4fba4279c27a4d2e301
meruem96 <clement.legoupil@gmail.com>
2021-03-11 12:33:35 +0100

    change outfile

--- setup.sh
+++ setup.sh
@@ -4,21 +4,23 @@ export TERRAGOAT_STATE_STORAGE_ACCOUNT="terragoatmodsa"
 export TERRAGOAT_STATE_CONTAINER="mydevsecops"
 export TF_VAR_environment="dev"
 export TF_VAR_region="westus"
+outfile=".log/"
+
 
 # Create resource group
 echo -n "Resource group ..."
-az group create --location $TF_VAR_region --name $TERRAGOAT_RESOURCE_GROUP >> .log/setupoutput.log && echo "OK"
-
+az group create --location $TF_VAR_region --name $TERRAGOAT_RESOURCE_GROUP >> $outfile"/setupoutput.log" && echo "OK"
+exit
 # Create storage account
-az storage account create --name $TERRAGOAT_STATE_STORAGE_ACCOUNT --resource-group $TERRAGOAT_RESOURCE_GROUP --location $TF_VAR_region --sku Standard_LRS --kind StorageV2 --https-only true --encryption-services blob >> .log/setupoutput.log && echo "Storage account ...OK"
+az storage account create --name $TERRAGOAT_STATE_STORAGE_ACCOUNT --resource-group $TERRAGOAT_RESOURCE_GROUP --location $TF_VAR_region --sku Standard_LRS --kind StorageV2 --https-only true --encryption-services blob >> $outfile"setupoutput.log" && echo "Storage account ...OK"
 
 # Get storage account key
 echo -n "Storage account key ..."
-ACCOUNT_KEY=$(az storage account keys list --resource-group $TERRAGOAT_RESOURCE_GROUP --account-name $TERRAGOAT_STATE_STORAGE_ACCOUNT --query [0].value -o tsv) >> .log/setupoutput.log && echo "OK"
+ACCOUNT_KEY=$(az storage account keys list --resource-group $TERRAGOAT_RESOURCE_GROUP --account-name $TERRAGOAT_STATE_STORAGE_ACCOUNT --query [0].value -o tsv) >> $outfile"setupoutput.log" && echo "OK"
 
 # Create blob container
 echo -n "Blob container ..."
-az storage container create --name $TERRAGOAT_STATE_CONTAINER --account-name $TERRAGOAT_STATE_STORAGE_ACCOUNT --account-key $ACCOUNT_KEY >> .log/setupoutput.log && echo "OK"
+az storage container create --name $TERRAGOAT_STATE_CONTAINER --account-name $TERRAGOAT_STATE_STORAGE_ACCOUNT --account-key $ACCOUNT_KEY >> $outfile"setupoutput.log" && echo "OK"
 
 # Fetch object_id
 objectId=$(az ad signed-in-user show --query objectId)
@@ -40,7 +42,7 @@ echo -n "Terraform init ..."
 terraform init -reconfigure -backend-config="resource_group_name=$TERRAGOAT_RESOURCE_GROUP" \
     -backend-config "storage_account_name=$TERRAGOAT_STATE_STORAGE_ACCOUNT" \
     -backend-config="container_name=$TERRAGOAT_STATE_CONTAINER" \
-    -backend-config "key=$TF_VAR_environment.terraform.tfstate" >> .log/setupoutput.log && echo "OK"
+    -backend-config "key=$TF_VAR_environment.terraform.tfstate" >> $outfile"setupoutput.log && echo "OK"
 
 
 if ! [ -d .logs ]; then
