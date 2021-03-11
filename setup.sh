4426cd3c30c35a1cfc8b6367eb1cb6d299e78f17
meruem96 <clement.legoupil@gmail.com>
2021-03-11 11:05:53 +0100

    change log directory + update tries tfplan

--- setup.sh
+++ setup.sh
@@ -7,18 +7,18 @@ export TF_VAR_region="westus"
 
 # Create resource group
 echo -n "Resource group ..."
-az group create --location $TF_VAR_region --name $TERRAGOAT_RESOURCE_GROUP >> setupoutput.log && echo "OK"
+az group create --location $TF_VAR_region --name $TERRAGOAT_RESOURCE_GROUP >> .log/setupoutput.log && echo "OK"
 
 # Create storage account
-az storage account create --name $TERRAGOAT_STATE_STORAGE_ACCOUNT --resource-group $TERRAGOAT_RESOURCE_GROUP --location $TF_VAR_region --sku Standard_LRS --kind StorageV2 --https-only true --encryption-services blob >> setupoutput.log && echo "Storage account ...OK"
+az storage account create --name $TERRAGOAT_STATE_STORAGE_ACCOUNT --resource-group $TERRAGOAT_RESOURCE_GROUP --location $TF_VAR_region --sku Standard_LRS --kind StorageV2 --https-only true --encryption-services blob >> .log/setupoutput.log && echo "Storage account ...OK"
 
 # Get storage account key
 echo -n "Storage account key ..."
-ACCOUNT_KEY=$(az storage account keys list --resource-group $TERRAGOAT_RESOURCE_GROUP --account-name $TERRAGOAT_STATE_STORAGE_ACCOUNT --query [0].value -o tsv) >> setupoutput.log && echo "OK"
+ACCOUNT_KEY=$(az storage account keys list --resource-group $TERRAGOAT_RESOURCE_GROUP --account-name $TERRAGOAT_STATE_STORAGE_ACCOUNT --query [0].value -o tsv) >> .log/setupoutput.log && echo "OK"
 
 # Create blob container
 echo -n "Blob container ..."
-az storage container create --name $TERRAGOAT_STATE_CONTAINER --account-name $TERRAGOAT_STATE_STORAGE_ACCOUNT --account-key $ACCOUNT_KEY >> setupoutput.log && echo "OK"
+az storage container create --name $TERRAGOAT_STATE_CONTAINER --account-name $TERRAGOAT_STATE_STORAGE_ACCOUNT --account-key $ACCOUNT_KEY >> .log/setupoutput.log && echo "OK"
 
 # Fetch object_id
 objectId=$(az ad signed-in-user show --query objectId)
@@ -40,19 +40,19 @@ echo -n "Terraform init ..."
 terraform init -reconfigure -backend-config="resource_group_name=$TERRAGOAT_RESOURCE_GROUP" \
     -backend-config "storage_account_name=$TERRAGOAT_STATE_STORAGE_ACCOUNT" \
     -backend-config="container_name=$TERRAGOAT_STATE_CONTAINER" \
-    -backend-config "key=$TF_VAR_environment.terraform.tfstate" >> setupoutput.log && echo "OK"
+    -backend-config "key=$TF_VAR_environment.terraform.tfstate" >> .log/setupoutput.log && echo "OK"
 
 
-if ! [ -d .plan ]; then
-    mkdir .plan
+if ! [ -d .logs ]; then
+    mkdir .logs
 fi
 echo -n "Exporting plan ..."
-terraform plan > .plan/plan && echo "OK"
+terraform plan > .logs/tfplan && echo "OK"
 
 read -p "Apply ? (Launch scripts = create the environement) [Y/N] " resp
 if [ "$resp" == "Y" ] || [ "$resp" == "y" ] || [ "$resp" == "yes" ] || [ "$resp" == "Yes" ]
 then
-    terraform apply ".plan/plan"
+    terraform apply .logs/tfplan
 
 fi
 
