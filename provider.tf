78818f8df5bbe0133c1b6d80c0c8d50bc822c5b2
meruem96 <clement.legoupil@gmail.com>
2021-03-11 09:35:58 +0100

    delete warning message

--- provider.tf
+++ provider.tf
@@ -1,12 +1,16 @@
 provider "azurerm" {
   subscription_id = var.subscription_id
-  version = ">= 2.0.0"
   features {}
 }
+
 
 data "azurerm_client_config" "current" {}
 
 terraform {
+  required_providers {
+    source = "hashicorp/azurerm"
+    version = ">= 2.0.0"
+  }
   required_version = ">=0.12.0"
   backend "azurerm" {
   }
