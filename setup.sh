364331390738cbad542207cedcd43e70376b0a95
meruem96 <clement.legoupil@gmail.com>
2021-03-15 11:56:21 +0100

    handle log-profiles error

--- setup.sh
+++ setup.sh
@@ -84,5 +84,7 @@ then
     az group delete --resource-group $TERRAGOAT_RESOURCE_GROUP --yes
     az group delete --resource-group "terragoat-"$TF_VAR_environment --yes 
     az group delete --resource-group "NetworkWatcherRG" --yes
+    log_profile_name=$(cat .logs/clearTextplan.log | grep -A10 'log_profil' | grep 'name' | tr -d ' ' | cut -d'=' -f2)
+    az monitor log-profiles delete --name $log_profile_name
 fi
 
