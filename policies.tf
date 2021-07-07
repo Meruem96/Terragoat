3af645108ecbfd740d9eee6b127e427bbb348886
Meruem96 <61518622+Meruem96@users.noreply.github.com>
2021-07-07 15:15:15 +0200

    Update policies.tf

--- policies.tf
+++ policies.tf
@@ -37,7 +37,7 @@ PARAMETERS
 
 resource "azurerm_subscription_policy_assignment" "example" {
   name                 = "terragoat-policy-assignment-${var.environment}"
-  subscription_id      = "/subscription/${subscription_id}"
+  subscription_id      = "{data.azurerm_subscription.current_subscription.id}"
   policy_definition_id = azurerm_policy_definition.policy-res.id
   description          = "Policy Assignment created via an Acceptance Test"
   display_name         = "My Example Policy Assignment"
