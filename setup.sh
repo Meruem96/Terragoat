919c70c612696b83858549441b3e1dc90025c51b
Meruem96 <61518622+Meruem96@users.noreply.github.com>
2021-11-29 13:30:48 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export TERRAGOAT_RESOURCE_GROUP="RG_TP_Azure_Hardening"
+export TERRAGOAT_RESOURCE_GROUP="RG_TP_Azure_Hardening_02"
 export TERRAGOAT_STATE_CONTAINER="mydevsecops"
 export TERRAGOAT_STATE_STORAGE_ACCOUNT="terragoatmodsa"
 export TF_VAR_environment="dev"
