b12a3bc2553e99da0d7b5ed4b0a52c28e6775d2b
Meruem96 <61518622+Meruem96@users.noreply.github.com>
2021-11-29 14:17:30 +0100

    Update app_service.tf

--- app_service.tf
+++ app_service.tf
@@ -1,7 +1,7 @@
 resource azurerm_app_service_plan "example" {
   name                = "terragoat-app-service-plan-${var.environment}"
   location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = var.rg_name
 
   sku {
     tier = "Dynamic"
@@ -13,7 +13,7 @@ resource azurerm_app_service "app-service1" {
   app_service_plan_id = azurerm_app_service_plan.example.id
   location            = var.location
   name                = "terragoat-app-service-${var.environment}${random_integer.rnd_int.result}"
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = var.rg_name
   https_only          = false
   site_config {
     min_tls_version = "1.1"
@@ -24,7 +24,7 @@ resource azurerm_app_service "app-service2" {
   app_service_plan_id = azurerm_app_service_plan.example.id
   location            = var.location
   name                = "terragoat-app-service-${var.environment}${random_integer.rnd_int.result}"
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = var.rg_name
   https_only          = true
 
   auth_settings {
