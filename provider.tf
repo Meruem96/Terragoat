c593e47424f331d6d2cd304d8961593da7bb2d41
Meruem96 <61518622+Meruem96@users.noreply.github.com>
2021-11-29 10:52:48 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,25 @@
 provider "azurerm" {
-  features {}
+  subscription_id = var.subscription_id
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy = true
+    }
+  }
+}
+
+
+# Fetch current user info using the az cli
+# Not possible to get the object_id of current user
+# Terraform retreive object_id via command execution
+# command ex: az ad signed-in-user show --query "{key:value}"
+data "external" "user" {
+  program = ["az", "ad", "signed-in-user", "show", "--query", "{displayName: displayName,objectId: objectId,objectType: objectType,upn: upn}"]
+}
+
+data "azurerm_client_config" "current" {}
+
+terraform {
+  required_version = ">=0.12.0"
+  backend "azurerm" {
+  }
 }
