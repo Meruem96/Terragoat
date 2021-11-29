1b3817311c61396d07b3268d0a8435c9a5c6664f
Meruem96 <61518622+Meruem96@users.noreply.github.com>
2021-11-29 11:02:44 +0100

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export TERRAGOAT_RESOURCE_GROUP="RG_TP_Azure_Hardening"
+export TERRAGOAT_STATE_CONTAINER="mydevsecops"
+export TERRAGOAT_STATE_STORAGE_ACCOUNT="terragoatmodsa"
+export TF_VAR_environment="dev"
+export TERRAGOAT_RESOURCE_GROUP_ID=`az group show --name $TERRAGOAT_RESOURCE_GROUP --query id --output tsv`
+export TF_VAR_region="francecentral"
