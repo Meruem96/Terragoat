b47d99d3ad9a57b0a3e75377a39cc295bad08fd8
Meruem96 <61518622+Meruem96@users.noreply.github.com>
2021-11-29 14:19:41 +0100

    Update networking.tf

--- networking.tf
+++ networking.tf
@@ -2,12 +2,12 @@ resource "azurerm_virtual_network" "example" {
   name                = "terragoat-vn-${var.environment}"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = var.rg_name
 }
 
 resource "azurerm_subnet" "example" {
   name                 = "terragoat-${var.environment}"
-  resource_group_name  = azurerm_resource_group.example.name
+  resource_group_name  = var.rg_name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.0.0/24"]
 }
@@ -15,7 +15,7 @@ resource "azurerm_subnet" "example" {
 resource "azurerm_network_interface" "ni_linux" {
   name                = "terragoat-linux-${var.environment}"
   location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = var.rg_name
 
   ip_configuration {
     name                          = "internal"
@@ -27,7 +27,7 @@ resource "azurerm_network_interface" "ni_linux" {
 resource "azurerm_network_interface" "ni_win" {
   name                = "terragoat-win-${var.environment}"
   location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = var.rg_name
 
   ip_configuration {
     name                          = "internal"
@@ -39,7 +39,7 @@ resource "azurerm_network_interface" "ni_win" {
 resource "azurerm_network_security_group" "bad_sg" {
   location            = azurerm_resource_group.example.location
   name                = "terragoat-${var.environment}"
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = var.rg_name
 
   security_rule {
     access                 = "Allow"
@@ -74,14 +74,14 @@ resource "azurerm_subnet_network_security_group_association" "nsg_link" {
 resource "azurerm_network_watcher" "network_watcher" {
   location            = azurerm_resource_group.example.location
   name                = "terragoat-network-watcher-${var.environment}"
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = var.rg_name
 }
 
 resource "azurerm_network_watcher_flow_log" "flow_log" {
   enabled                   = true
   network_security_group_id = azurerm_network_security_group.bad_sg.id
   network_watcher_name      = azurerm_network_watcher.network_watcher.name
-  resource_group_name       = azurerm_resource_group.example.name
+  resource_group_name       = var.rg_name
   storage_account_id        = azurerm_storage_account.example.id
   retention_policy {
     enabled = false
