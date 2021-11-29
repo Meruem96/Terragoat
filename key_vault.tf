e99d6c8a96474d388388bb4914b58f89a8b22500
Meruem96 <61518622+Meruem96@users.noreply.github.com>
2021-11-29 14:18:58 +0100

    Update key_vault.tf

--- key_vault.tf
+++ key_vault.tf
@@ -1,7 +1,7 @@
 resource "azurerm_key_vault" "example" {
   name                     = "terragoat-key-${var.environment}${random_integer.rnd_int.result}"
   location                 = azurerm_resource_group.example.location
-  resource_group_name      = azurerm_resource_group.example.name
+  resource_group_name      = var.rg_name
   tenant_id                = data.azurerm_client_config.current.tenant_id
   purge_protection_enabled = false
   sku_name                 = "premium"  
