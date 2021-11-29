f6fb262fedf2a8a4f7d8fec5c518170e02269985
Meruem96 <61518622+Meruem96@users.noreply.github.com>
2021-11-29 14:14:17 +0100

    Update networking.tf

--- networking.tf
+++ networking.tf
@@ -2,5 +2,5 @@ resource "azurerm_virtual_network" "example" {
   name                = "terragoat-vn-1"
   address_space       = ["10.0.0.0/16"]
   location            = var.location
-  resource_group_name = azurerm_resource_group.name
+  resource_group_name = azurerm_resource_group.example.name
 }
