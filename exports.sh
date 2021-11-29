6de88d2714e67abe8451aa65079e046711934eb9
Meruem96 <61518622+Meruem96@users.noreply.github.com>
2021-11-29 10:27:20 +0100

    Delete exports.sh

--- exports.sh
+++
@@ -1,5 +0,0 @@
-export TERRAGOAT_RESOURCE_GROUP="RG_TP_Azure_Hardening"
-export TERRAGOAT_STATE_CONTAINER="mydevsecops"
-export TERRAGOAT_STATE_STORAGE_ACCOUNT="terragoatmodsa"
-export TF_VAR_environment="dev"
-export TF_VAR_region="francecentral"
