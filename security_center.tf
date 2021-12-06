e83b35d0a518d200664c4f2498c9c740850653f6
Meruem96 <61518622+Meruem96@users.noreply.github.com>
2021-12-06 15:58:00 +0100

    Update security_center.tf

--- security_center.tf
+++ security_center.tf
@@ -1,7 +1,3 @@
-resource azurerm_security_center_subscription_pricing "pricing" {
-  tier = "Free"
-}
-
 resource azurerm_security_center_contact "contact" {
   alert_notifications = false
   alerts_to_admins    = false
