c56c233c52d73a81dd9faf73ef964e7f42561694
Meruem96 <clement.legoupil@gmail.com>
2022-04-04 18:02:57 +0200

    update

--- sql.tf
+++ sql.tf
@@ -49,7 +49,7 @@ resource "azurerm_mysql_server" "example" {
   infrastructure_encryption_enabled = false
   public_network_access_enabled     = true
   ssl_enforcement_enabled           = false
-  ssl_minimal_tls_version_enforced = "TLS1_1"
+  ssl_minimal_tls_version_enforced = "TLSEnforcementDisabled"
 }
 
 resource "azurerm_postgresql_server" "example" {
